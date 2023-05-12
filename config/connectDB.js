cf3d28da652f5ecce0c3301a0e2adb8322fc0e70
shaikat17 <shaikatpal56@gmail.com>
2023-05-12 17:59:18 +0600

    task created route made

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const uri = "mongodb://127.0.0.1:27017";
+const uri = "mongodb://127.0.0.1:27017/task-manager";
 
 const connectDB = async () => {
   try {
--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb")
 const app = express();
 
-const connectDB = require('./config/connectDB.js')
+const connectDB = require('./config/connectDB.js');
+const Task = require("./model/taskModel.js");
 
 const port = process.env.PORT || 3000
 
+app.use(express.json())
 
 
 connectDB()
@@ -13,6 +15,12 @@ connectDB()
 app.get("/", (req, res) => {
   res.send("hello");
 });
+
+app.post('/api/tasks' , async (req, res) => {
+  const data = req.body
+  const result = await Task.create(data)
+  res.send(result)
+})
 
 
 app.listen(port, () => {
